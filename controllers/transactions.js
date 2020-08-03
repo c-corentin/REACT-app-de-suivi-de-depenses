45c1b7538b3d22870cf45622f01829d845791349
Corentin <corentin.colart@gmail.com>
2020-08-03 15:25:26 +0200

    Transaction route and controller

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,3 +1,7 @@
+//DESC : Get all transactions
+// ROUTE : GET /api/v1/transactions
+// ACCESS : Public
+
 exports.getTransactions = (req, res, next) =>  {
-res.send();
+res.send('GET transactions');
 };
