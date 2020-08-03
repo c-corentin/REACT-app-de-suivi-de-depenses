1414140d8e966d8230104718ca3a3a6b480d4f38
Corentin <corentin.colart@gmail.com>
2020-08-03 15:38:52 +0200

    controller and routes update

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,7 +1,23 @@
-//DESC : Get all transactions
+// DESC : Get all transactions
 // ROUTE : GET /api/v1/transactions
 // ACCESS : Public
 
 exports.getTransactions = (req, res, next) =>  {
 res.send('GET transactions');
+};
+
+// DESC : Add  transaction
+// ROUTE : POST /api/v1/transactions
+// ACCESS : Public
+
+exports.addTransactions = (req, res, next) =>  {
+    res.send('POST transactions');
+};
+
+// DESC : Delete transaction
+// ROUTE : DELETE /api/v1/transactions/:id
+// ACCESS : Public
+
+exports.deleteTransactions = (req, res, next) =>  {
+    res.send('DELETE transactions');
 };
--- routes/transactions.js
+++ routes/transactions.js
@@ -2,10 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
-const {getTransactions} = require('../controllers/transactions');
+const {getTransactions, addTransactions, deleteTransactions} = require('../controllers/transactions');
 
 router
     .route('/')
-    .get(getTransactions);
+    .get(getTransactions)
+    .post(addTransactions);
+
+router
+    .route('/:id')
+    .delete(deleteTransactions);
 
 module.exports = router;
